07d1515aa86c9d391b2ebba5c4139bc18e3c7bcf
Subhasish panda <subhasish418@gmail.com>
2022-08-21 15:26:01 +0530

    gitignore created

---
+++ array/sum_of_array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[10],sum=0;
+    cout << "Enter 10 numbers :" << endl;
+    for(int i=0;i<10;i++)
+        cin >> arr[i];
+
+    for(int i=0;i<10;i++)
+        sum += arr[i];
+    cout << "Sum is : " << sum ;
+    return 0;
+}
